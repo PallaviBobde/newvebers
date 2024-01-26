fb1b53974a8dfc06e25348ef27b3894053d12479
gagan-baghel <gagansinghbaghel15@gmail.com>
2024-01-26 06:14:11 -0800

    contact page layout

---
+++ src/components/SocialLinkCard/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <a className='rounded-[10px] border border-[#8892B070] bg-[#1e90ff20] p-4 '>
+        <div className="text-[12px] text-[#828285]">Hello MAN</div>
+        <div className="flex justify-between">
+            <p>gaganbaghel@gmail.com </p>
+            <i class="fa-solid fa-arrow-right-long "></i>
+        </div>
+    </a>
+  )
+}
