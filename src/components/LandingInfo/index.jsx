937763ec1f6e97046f031871acfe1f35fef39fb9
gagan-baghel <gagansinghbaghel15@gmail.com>
2024-01-27 07:15:38 -0800

    updated home page

--- src/components/LandingInfo/index.jsx
+++ src/components/LandingInfo/index.jsx
@@ -3,11 +3,12 @@ import './style.css'
 import ContactForm from "../ContactForm/HomePageContact";
 
 function LandingInfo() {
-  return <div className="landingInfo">
+  return <div className="landingInfo h-[90vh]">
     <h1>Build your next Website with us.</h1>
     {/* <p>We at VebersZone are dedicated to bringing a new era of design to the web. We want our customers to have web experiences they can be proud of.</p> */}
     <ContactForm/>
-    <img className="img" src="https://cdn.pixabay.com/photo/2016/04/27/01/39/search-1355847_1280.png"/>
+    <i class="fas fa-angle-down absolute bottom-0"></i>
+
   </div>;
 }
 
