9f2adaaae6aa6e9f29deef54410623764ccc95a0
pallavi bobde <pallavi.bobde@bajajfinserv.in>
2024-01-31 23:31:12 +0530

    contact page

--- src/components/ContactForm/genralContactForm/index.jsx
+++ src/components/ContactForm/genralContactForm/index.jsx
@@ -14,45 +14,37 @@ export default function index() {
   return (
     <section
       id="Contact"
-      className="relative py-4 flex flex-col sm:px-28 sm:pb-36 pb-8 text-white"
+      className="relative py-4 flex flex-col sm:px-28 sm:pb-36 pb-8 text-white "
     >
-      <div className=" flex-col my-12 bg-[#82828520] p-8 rounded-[10px] hideonphone">
-        <h2 className="text-[4rem] ">Have a question or want to get in touch</h2>
-        <p className="px-4 my-4 text-[1.2rem] text-[#1e8fffe4]">
-          We would love to hear from you! Feel free to reach out through any
-          of the following methods
-        </p>
-      </div>
+      {/* <div className="w-full z-10 flex  "> */}
 
-      <div className=" flex-col space-y-2 my-6 bg-[#82828520] p-10 rounded-[10px] hideonphone">
-        <p className="text-[1.4rem] px-2">LOCATIONS AND MAPs </p>
-        <div className="flex p-8">
-          <LocationCard />
-        </div>
-      </div>
-      <div className="w-full lg:flex-row flex-col flex z-10 bg-[#82828520] p-8 rounded-[10px]">
-
-        <div className="lg:w-1/2 w-full p-24 flex flex-col flex-1 text-white space-y-10 justify-around">
-          <div className="flex flex-col space-y-2 ">
-            <div className="text-[2.5rem]">Let's Chat </div>
-            <div className="px-2 text-[1.2rem] text-[#1e8fffe4]">
+        {/* <div className="lg:w-1/2 w-full my-12  flex flex-col flex-1 text-white space-y-10 "> */}
+          <div className="flex flex-col space-y-2 mb-10 mt-10">
+            <div className="text-[2.5rem]">Have a project to discuss ? Let's get in touch! </div>
+            <div className="px-2 text-[1.2rem] ">
            <span className="text-[#ffdb4d]"> Warning: </span>  We may make your competitors jealous... So connect with us at your own risk !!
             </div>
           </div>
 
-          <div className="flex space-x-3">
+          <div className="flex space-x-3 mb-10">
             <MediaButton contactLink={"https://www.linkedin.com/in/vebers-zone-a50b242aa/"} iconName={"fa fa-linkedin"} />
             <MediaButton contactLink={"https://www.instagram.com/veberszone"} iconName={"fa fa-instagram"}/>
             <MediaButton contactLink={whatsappUrl} iconName={"fa fa-whatsapp"}  />
           </div>
-          <div className="flex space-y-6 flex-col">
+          <div className=" flex space-y-6 flex-col">
+            <Link
+                to="tel:+918319699485"
+                className="contact_link"
+              >
+                +91 8319699485
+              </Link>
             <SocialLinkCard contactLink={"Mailto:veberszone@gmail.com"} iconName={"fa-envelope"} heading="Mail" content="veberszone@gmail.com"/>
             <SocialLinkCard contactLink={whatsappUrl} iconName={"fa-whatsapp"} heading="Whatsapp US: " content="+91 831 9699 485"/>
             <SocialLinkCard contactLink={"tel:8319699485"} iconName={"fa-call"} heading="Call Us" content="+91 831 9699 485"/>
           </div>
-        </div>
-        <ContactCard/>
-      </div>
+        {/* </div> */}
+        {/* <ContactCard/> */}
+      {/* </div> */}
     </section>
   );
 }
