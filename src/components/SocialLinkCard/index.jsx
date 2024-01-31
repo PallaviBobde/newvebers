1db77298dfc21247b3ee1b6c91929efe9186a23e
gagan-baghel <gagansinghbaghel15@gmail.com>
2024-01-31 10:38:05 -0800

    res done

--- src/components/SocialLinkCard/index.jsx
+++ src/components/SocialLinkCard/index.jsx
@@ -5,7 +5,7 @@ export default function index({heading, content, contactLink}) {
     <a href={contactLink} target='_blank'  className='rounded-[10px] border border-[#8892B070] bg-[#111111] p-4 '>
         <div className="text-[1rem] text-[#1e8fffe4]">{heading}</div>
         <div className="flex justify-between text-[1.2rem]">
-            <p className="">{content} </p>
+            <p className="truncate">{content} </p>
             <i className="fa-solid fa-arrow-right-long "></i>
         </div>
     </a>
